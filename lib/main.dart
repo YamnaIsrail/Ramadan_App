7e22f0eaa68eef92117e066b6b44e3a3a93974ae
yum <yamnaisrailkhan@gmail.com>
2024-02-16 18:27:12 +0500

    Firebase initialization in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:ramadan_app/Screens/Home_Screen/Home_Screen.dart';
 import 'Screens/StarterScreens/SplashScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyDlnNmA1eOTssQ9ZigvGYw59ln7ZJmrwXQ",
+      authDomain: "",
+      projectId: "ramadanapp-2457e",
+      storageBucket: "",
+      messagingSenderId: "450704756196",
+      appId: "1:450704756196:android:2bb813201c6c36d780cbf1",
+    ),
+  ); // Initialize Firebase
+
+  runApp( MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
@@ -11,13 +28,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ramadan App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Color(0xffd6a75f)),
         useMaterial3: true,
       ),
-      home: SplashScreen(),
+      home: HomeScreen(),
     );
   }
 }
